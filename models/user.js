25793eaa17a8d10d12c24a1a4fe2592e7e2abce5
Eric-Ta <56466438+Eric-Ta@users.noreply.github.com>
2022-03-29 17:45:23 -0600

    created some more db schemas + did the linking between schema

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const UserSchema = new mongoose.Schema({
     email: {
@@ -15,12 +16,90 @@ const UserSchema = new mongoose.Schema({
         default: false,
         required: true
     },
-    messages: [{message: String, date: Date, hidden: Boolean}]
+    messages: [{
+        type: Schema.Types.ObjectId,
+        ref: "Feedback",
+        // message: String, 
+        // date: Date, 
+        // hidden: Boolean
+    }]
     // TODO: move messages to separate database
 }, {
     timestamps: true
 });
 
+const CourseSchema = new Schema({
+    course: { 
+        type: String, // Example of input structure: DEPT NUM eg. SENG 513. may need to verify somewhere that it is in this format 
+        required: true,
+        index: true
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+  
+    messages: [{
+        type: Schema.Types.ObjectId,
+        ref: "Feedback"
+    }]
+    // messages: [{message: String, date: Date, hidden: Boolean}]
+    // This might need to be connected to the comments table 
+}, {
+    timestamps: true
+});
+
+const FeedbackSchema = new Schema({
+    feedback_id: {
+        type: INT,
+        required: true,
+        index: true
+    },
+   /*  comment: {
+        type: String,
+        required: false,
+    }, */
+    comment: {message: String, date: Date, hidden: Boolean},
+
+    rating: { //stars out of 5
+        type: INT, //might change depending on how starts is implemented
+        required: false,
+    },
+    comment_rating: { //upvotes/downvotes on that comment
+        type: INT,
+        required: true,
+    },
+
+    course: {
+        type: Schema.Types.ObjectId,
+        ref: "Course"
+    }
+    //need to have a link to the course this feedback is being provided for 
+}, {
+    timestamps: true
+});
+
+const DepartmentSchema = new Schema({
+    department: {
+        type: String,
+        required: true,
+        index: true
+    },
+   
+    //holds the courses that belong to the department from CoursesSchema
+    course: [{
+        type: Schema.Types.ObjectId,
+        ref: "Course"
+    }]
+}, {
+    timestamps: true
+});
+
+
 const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
+
+module.exports = mongoose.model('Course', CourseSchema);
+module.exports = mongoose.model('Feedback', FeedbackSchema);
+module.exports = mongoose.model('Department', DepartmentSchema);
