7f4b6d2f280c817c6d936fb82337ce37bc4acbb5
Eric-Ta <56466438+Eric-Ta@users.noreply.github.com>
2022-04-04 17:06:11 -0600

    load db departments into search bar

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const Course = require('../models/course')
+const Department = require('../models/department')
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', async (req, res) => {
     const courses = await Course.find()
-    res.render(('index.ejs'), { Courses: courses});
+    const departments = await Department.find();
+    res.render(('index.ejs'), { Courses: courses, Departments: departments});
+   // res.render(('index.ejs'), { Departments: departments}); CANNOT USE 
 });
 
 module.exports = indexRouter;
