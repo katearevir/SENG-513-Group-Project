e9afd6457748d65a0906845250625c16b33871ec
Eric-Ta <56466438+Eric-Ta@users.noreply.github.com>
2022-04-04 17:30:34 -0600

    added sorting to the course list and department list

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -5,8 +5,8 @@ const Department = require('../models/department')
 const indexRouter = express.Router();
 
 indexRouter.get('/', async (req, res) => {
-    const courses = await Course.find()
-    const departments = await Department.find();
+    const courses = await Course.find().sort({"course":1})
+    const departments = await Department.find().sort({"department":1});
     res.render(('index.ejs'), { Courses: courses, Departments: departments});
    // res.render(('index.ejs'), { Departments: departments}); CANNOT USE 
 });
