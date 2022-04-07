c1bb4bb9289da5fa9bce407f092ecf9eb6b616e1
Jack <ahfhafh@gmail.com>
2022-04-06 19:00:28 -0600

    combine index and mod page

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -5,14 +5,14 @@ const Department = require('../models/department')
 const indexRouter = express.Router();
 
 indexRouter.get('/', async (req, res) => {
-    if (res.locals.user) {
-        if (res.locals.user.isAdmin) {
-            return res.redirect('moderator');
-        };
-    }
     const courses = await Course.find().sort({ "course": 1 })
     const departments = await Department.find().sort({ "department": 1 });
-    res.render(('index.ejs'), { Courses: courses, Departments: departments });
+    if (res.locals.user) {
+        if (res.locals.user.isAdmin) {
+            return res.render(('index.ejs'), { Courses: courses, Departments: departments, isAdmin: true });
+        };
+    }
+    res.render(('index.ejs'), { Courses: courses, Departments: departments, isAdmin: false });
 });
 
 module.exports = indexRouter;
