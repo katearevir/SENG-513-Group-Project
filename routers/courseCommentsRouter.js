f60f7caf676d981bdfa42f3279db12993d73141b
Jack <ahfhafh@gmail.com>
2022-04-07 19:24:02 -0600

    combine course comments and course comments mod

--- routers/courseCommentsRouter.js
+++ routers/courseCommentsRouter.js
@@ -7,7 +7,12 @@ const courseCommentsRouter = express.Router();
 courseCommentsRouter.get('/courseComments/:coursename', async (req, res) => {
     const course = await courseModel.findOne({ course: req.params.coursename}).lean();
     const reviews = await FeedbackModel.find({ course: course._id });
-    res.render(('courseComments.ejs'), { Reviews: reviews });
+    if (res.locals.user) {
+        if (res.locals.user.isAdmin) {
+            return res.render(('courseComments.ejs'), { Reviews: reviews, isAdmin: true});
+        };
+    }
+    res.render(('courseComments.ejs'), { Reviews: reviews, isAdmin: false });
 });
 
 module.exports = courseCommentsRouter;
