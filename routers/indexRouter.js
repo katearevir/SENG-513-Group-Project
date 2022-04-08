e3d90e4149ddafe1545d58a47608a41cfee597cd
Jack <ahfhafh@gmail.com>
2022-04-08 02:29:55 -0600

    avg ratings

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -6,14 +6,23 @@ const FeedbackModel = require('../models/feedback')
 const indexRouter = express.Router();
 
 indexRouter.get('/', async (req, res) => {
-    const courses = await CourseModel.find().sort({ "course": 1 })
+    const courses = await CourseModel.aggregate([
+        {
+            "$lookup": {
+                "from": "feedbacks",
+                "localField": "messages",
+                "foreignField": "_id",
+                "as": "messages"
+            }
+        },
+        {
+            "$addFields": {
+                "avgRating": { "$avg": "$messages.rating" }
+            }
+        }
+    ]).sort({ "avgRating": -1 });
+
     const departments = await DepartmentModel.find().sort({ "department": 1 });
-    const allRatings = await FeedbackModel.find({}, { rating: 1, _id: 0 });
-    let allRatingsTotal = 0;
-    for (let i = 0; i < allRatings.length; i++) {
-        allRatingsTotal += allRatings[i].rating;
-    }
-    const avgRating = allRatingsTotal / allRatings.length;
     if (res.locals.user) {
         if (res.locals.user.isAdmin) {
             return res.render(('index.ejs'), { Courses: courses, Departments: departments, isAdmin: true });
