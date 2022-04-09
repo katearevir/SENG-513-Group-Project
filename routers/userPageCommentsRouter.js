068c69bd4aa0fd72a233d1ff815abd99d03db2bd
Jack <ahfhafh@gmail.com>
2022-04-09 02:16:56 -0600

    user comments on user page

--- routers/userPageCommentsRouter.js
+++ routers/userPageCommentsRouter.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const path = require('path');
 const { requireLogin } = require('../middleware/authMiddleware');
 const userPageCommentsRouter = express.Router();
+const UserModel = require('../models/user');
 
 userPageCommentsRouter.get('/userPageComments', requireLogin, (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'userPageComments.html'));
+    UserModel.findOne({ _id: res.locals.user.id }).populate({ path: 'messages', populate: { path: 'course' }}).exec((err, user) => {
+        if (err) return handleError(err);
+        res.render(('userPageComments.ejs'), { user: user, isAdmin: false });
+    });
+
 });
 
 module.exports = userPageCommentsRouter;
